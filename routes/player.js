cc5ee1973e74ad5ff294d312a90c64286c46d509
furo <urof@disroot.org>
2023-12-08 22:47:38 +0100

    Add json route for `players/`

--- routes/player.js
+++ routes/player.js
@@ -71,5 +71,32 @@ async function players_rank1s_route(req, res) {
 
 routes.get("/player/:player/:type(rank1s)", players_rank1s_route)
 
+async function players_json(req, res) {
+    const player = req.params.player
+    const rankings = groupBy(await Player.rankings(player), "Category")
+    if(!Object.keys(rankings).length)
+        return res.render("pages/player/base.njk")
+
+    const rankedPointsGraph = await Player.rankedpointsGraph(player)
+    const pointsGraph = await Player.pointsGraph(player)
+    const playtime = await Player.playtime(player)
+    const points = await Player.points(player)
+
+    const recentPlaytime = await Player.recentPlaytime(player, 10)
+    const playtimeCategories = await Player.playtimeCategories(player)
+    const playtimeGametypes = await Player.playtimeGametypes(player)
+    const playtimeLocation = await Player.playtimeLocation(player)
+    const playtimePerMonth = await Player.playtimePerMonth(player)
+    const mostPlayedMaps = await Player.mostPlayedMaps(player, 10)
+
+    const rank1sPartners = await Player.rank1sPartners(player, 10)
+    const recentTop10s = await Player.recentTop10s(player, 10)
+    const AmountOfTop10Placements = await Player.AmountOfTop10Placements(player)
+    const allTop10s = await Player.allTop10s(player)
+
+    return res.json({ player, points, rankings, rankedPointsGraph, pointsGraph, playtime, recentPlaytime, playtimeCategories, playtimeGametypes, playtimeLocation, playtimePerMonth, mostPlayedMaps, allTop10s, AmountOfTop10Placements, rank1sPartners, recentTop10s })
+}
+
+routes.get("/player/:player/json", players_json)
 
 export default routes
