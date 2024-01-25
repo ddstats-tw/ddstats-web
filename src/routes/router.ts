189d119705df55ebfbfc0f59a7ab63a50b510be4
furo <urof@disroot.org>
2024-01-25 14:50:10 +0100

    Add 404

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -7,12 +7,16 @@ import miscRoutes from "./misc.js"
 const routes = Router()
 
 routes.get("/", (req: Request, res: Response) => {
-    res.render("pages/home.njk",  { "search": false })
+    res.render("pages/home.njk", { "search": false })
 })
 
 routes.use("/", leaderboardRoutes)
 routes.use("/", playerRoutes)
 routes.use("/", mapRoutes)
 routes.use("/", miscRoutes)
+
+routes.get('*', (req, res) => {
+    res.status(404).render("pages/404.njk", { "search": true })
+});
 
 export default routes
