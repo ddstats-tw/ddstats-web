8f0102e3861cda1a20d22c6e831d49ec369ee80b
dobrykafe <dobrykafe3@gmail.com>
2024-05-19 01:05:47 +0200

    more settings for database

--- src/database.rs
+++ src/database.rs
@@ -1,16 +1,19 @@
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use std::env;
 
-pub async fn init_db(db_url: String) -> Pool<Postgres> {
+pub async fn init_db() -> Pool<Postgres> {
     PgPoolOptions::new()
-        // The default connection limit for a Postgres server is 100 connections, minus 3 for superusers.
-        // Since we're using the default superuser we don't have to worry about this too much,
-        // although we should leave some connections available for manual access.
-        //
-        // If you're deploying your application with multiple replicas, then the total
-        // across all replicas should not exceed the Postgres connection limit.
-        .max_connections(50)
-        .min_connections(25)
-        .connect(&db_url)
+        .max_connections(
+            env::var("DATABASE_MAX_CONNECTIONS")
+                .map(|x| x.parse().expect("not a number"))
+                .unwrap_or(50),
+        )
+        .min_connections(
+            env::var("DATABASE_MIN_CONNECTIONS")
+                .map(|x| x.parse().expect("not a number"))
+                .unwrap_or(1),
+        )
+        .connect(&env::var("DATABASE_URL").expect("failed to get DATABASE_URL"))
         .await
         .expect("could not connect to database")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use database::init_db;
 use points::parse_points;
-use std::env;
 use tera::init_tera;
 
 mod database;
@@ -15,8 +14,7 @@ async fn main() {
     dotenvy::dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let db_url = env::var("DATABASE_URL").expect("failed to get DATABASE_URL");
-    let db = init_db(db_url).await;
+    let db = init_db().await;
     let tera = init_tera();
     let points = parse_points();
 
